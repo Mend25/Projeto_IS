a8dab82da43e5c46564b0af3adf1b11c02250d40
Felipe Santos <ffss@cin.ufpe.br>
2023-07-13 11:27:56 -0300

    Update game.asm

--- game.asm
+++ game.asm
@@ -7,20 +7,22 @@ _start:
 	xor ax, ax
     xor cx, cx
    	xor dx, dx
-    mov bx, 10
-	mov si, flag
+    mov bx, 100
 	
-	mov ah, 0
-	mov al, 13h
-	int 10h
-
-    mov dx, bx
-    add bx, 16
-	call load_pixel
+	call clear_screen
+    call load_bar
 
 	jmp done
-	
-load_pixel:
+
+load_bar:
+    mov si, flag
+    mov dx, bx
+    add bx, 16
+	call print_bar
+    sub bx, 16
+    ret
+
+print_bar:
 	lodsb
 	mov ah, 0ch
 	int 10h
@@ -30,12 +32,12 @@ load_pixel:
     .travel_by_image:
         inc cx
         cmp cx, 16
-        jne load_pixel
+        jne print_bar
         
         xor cx, cx
         inc dx
         cmp dx, bx
-        jne load_pixel
+        jne print_bar
         
         ret
 
@@ -45,38 +47,31 @@ read_key:
     cmp al, 0x0d
     je .done
 
-    cmp al, 'w'
-    je .up 
-    
-    cmp al, 's'
-    je .down 
-    
     cmp al, 'd'
-    je .right
+    je .down
     
     cmp al, 'a'
-    je .left 
+    je .up 
     
-
     jmp read_key
 
     .done:
         ret
     .up:
-        mov al, 'U'
-        call putchar
+        dec bx
+        mov dx, bx
+        add bx, 16
+
+        call print_bar
+
         jmp read_key
     .down:
-        mov al, 'D'
-        call putchar
-        jmp read_key
-    .left:
-        mov al, 'L'
-        call putchar
-        jmp read_key
-    .right:
-        mov al, 'R'
-        call putchar
+        inc bx
+        mov dx, bx
+        add bx, 16
+        
+        call print_bar
+
         jmp read_key
 
 getchar:
@@ -84,9 +79,10 @@ getchar:
     int 16h
     ret
 
-putchar:
-    mov ah, 0x0e
-    int 10h 
+clear_screen:
+    mov ah, 0
+	mov al, 13h
+	int 10h
     ret
 
 done:
