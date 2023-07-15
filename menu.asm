faca78bbd5a4a9f17eaa862d8714ce34ba0b0df8
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-07-15 11:43:08 -0300

    Update menu.asm

--- menu.asm
+++ menu.asm
@@ -2,7 +2,7 @@ menu:
     mov ah, 0 
     mov al, 13h
     int 10h
-    
+
     mov bl, 0xf 
     mov ah, 0eh
     mov bh, 0
@@ -12,13 +12,14 @@ menu:
     call print_loop
     xor ax, ax
     call waitStart
-    
+
+
     xor ax, ax
-    
+
     mov ah, 0 
     mov al, 12h
     int 10h
-    
+
 
 print_loop:
     lodsb
@@ -26,21 +27,54 @@ print_loop:
     je .done
     call putchar
     jmp print_loop
-    
+
     .done:
-        ret    
+        ret
 
 
 waitStart:
     call getchar_m
-    
+
     cmp al, 0x0d
     je .done
-    
+
+    cmp al, " "
+    je instructions
+
     jmp waitStart
-    
+
     .done:
         ret
+
+instructions:
+    xor ax, ax
+    mov ah, 0 
+    mov al, 12h
+    int 10h
+
+    xor ax, ax
+    xor si, si
+    mov si, message2
+    jmp .print
+
+    .print:
+        lodsb
+        cmp al, 0
+        je .waitButton
+        call putchar
+        jmp .print
+
+    .waitButton:
+        xor ax, ax
+
+        call getchar_m
+
+        cmp al, " "
+        je menu
+
+        jmp .waitButton
+
+
 
 endl:
     mov ah, 0x0a
@@ -48,7 +82,7 @@ endl:
     mov ah, 0x0d
     call putchar
     ret
-    
+
 putchar:
     mov ah, 0x0e
     int 10h
@@ -58,4 +92,3 @@ getchar_m:
     mov ah, 0x00 
     int 16h
     ret
-
