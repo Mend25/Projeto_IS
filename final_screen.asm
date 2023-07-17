ada6a3379bc3674ddf4784677ad3f3d03576a9ba
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-07-17 15:36:58 -0300

    Update final_screen.asm

--- final_screen.asm
+++ final_screen.asm
@@ -60,17 +60,5 @@ final_score:
     mov ax, dx
     add ax, '0'
     call put_score
-    call highest_score
         
-    ret
-
-highest_score:
-    mov ax, [counter]
-    cmp ax, [highest_score1]
-    ja .change_highest
-
-    .change_highest:
-        mov [highest_score1], ax
-        mov ax, [highest_score1]
-      
     ret
