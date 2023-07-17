f0dd4a4dbb1230405841b157128f85944a390df0
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-07-17 03:35:40 -0300

    Update menu.asm

--- menu.asm
+++ menu.asm
@@ -106,7 +106,7 @@ menu:
 	setText 1, 14, message1, darkGreenColor
 	setText 6, 5, play, darkGreenColor
 	setText 6, 27, instruction, darkGreenColor
-	setText 15, 12, highest, darkGreenColor
+	setText 15, 5, highest, darkGreenColor
 	
 	call first_cursor ; Inicia a aplicação
 	
